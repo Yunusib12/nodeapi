a17d982f187df37cbd31b0545f504a9f48c30c05
Ibrahim Yunus <younis.ib@gmail.com>
2020-05-27 04:28:09 -0400

    employee projects listed

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,33 +1,66 @@
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
-const dbEmployee = util.promisify(fs.readFile);
+const dbReader = util.promisify(fs.readFile);
+const dbEmployee = new Promise((resolve, reject) => resolve(dbReader("./db/employeeDb.json")));
+const dbProject = new Promise((resolve, reject) => resolve(dbReader("./db/projectDb.json")));
+const dbEmpProj = new Promise((resolve, reject) => resolve(dbReader("./db/employeeProject.json")));
 
 let dbController = {
 
     findAllEmployee: function (req, res) {
 
-        Promise.all([dbEmployee("./db/employeeDb.json")])
-            .then((datas) => {
-
-                let listEmployeeArray = JSON.parse(datas);
-                res.render("main", { listEmployeeArray });
-            })
+        dbEmployee
+            .then((datas) => res.render("main", { listEmployeeArray: JSON.parse(datas) }))
             .catch((err) => console.log(err));
 
     },
     getEmployeeDetail: function (req, res) {
 
-        Promise.all([dbEmployee("./db/employeeDb.json")])
+        let employeeProfile = {
+            details: [],
+            projects: []
+        };
+
+        Promise.all([dbEmployee, dbProject, dbEmpProj])
             .then((datas) => {
 
-                let employeeId = req.params.id;
-                let listEmployeeArray = JSON.parse(datas);
-                let employeeDetail = listEmployeeArray.filter((employee) => employee.id == employeeId);
+                let listEmployeeArray = JSON.parse(datas[0]);
+                let listProjectArray = JSON.parse(datas[1]);
+                let listEmployeeProject = JSON.parse(datas[2]);
 
-                res.render("employeedetail", { employeeDetail });
-            })
-            .catch((err) => console.log(err));
+                //Getting employee details 
+                let employeeId = req.params.id;
+                employeeProfile.details = listEmployeeArray.filter((employee) => employee.id == employeeId);
+                let listEmpProj = listEmployeeProject.filter((project) => project.employeeId == employeeId);
+
+                listEmpProj.forEach(id => listProjectArray.forEach((project) => (project.id == id.projectId) && employeeProfile.projects.push(project)));
+
+                console.log(employeeProfile.details, employeeProfile.projects);
+
+                res.render("employeedetail", {
+                    emplDetArr: employeeProfile.details,
+                    emplProArr: employeeProfile.projects
+                });
+
+            }).catch((err) => console.log(err));
+
+        // Promise.all([dbReader("./db/employeeDb.json")])
+        //     .then((datas) => {
+
+
+        //         let listProjectArray = Promise.all([dbReader("./db/projectDb.json")])
+        //             .then((projects) => projects)
+        //             .catch((err) => console.log(err));
+
+        //         let projectDetail = 
+        //         listProjectArray.then((results) => {
+        //             console.log(JSON.parse(results));
+        //         })
+
+        //         res.render("employeedetail", { employeeDetail });
+        //     })
+        //     .catch((err) => console.log(err));
 
     }
 };
